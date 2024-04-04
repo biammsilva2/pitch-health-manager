c133e3c35123508c2732d66aeffc2b81b60eec13
Beatriz da Silva <beatriz.dasilva@zageno.com>
2024-04-04 23:02:28 +0200

    Adding update and delete method

--- pitch-health/schema.py
+++ pitch-health/schema.py
@@ -1,9 +1,37 @@
 from datetime import datetime
 from enum import Enum
-from typing import Optional
+from typing import Optional, Any
 from bson import ObjectId
 
 from pydantic import BaseModel, Field
+from pydantic_core import core_schema
+
+
+class PyObjectId(str):
+    @classmethod
+    def __get_pydantic_core_schema__(
+            cls, _source_type: Any, _handler: Any
+    ) -> core_schema.CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.union_schema([
+                core_schema.is_instance_schema(ObjectId),
+                core_schema.chain_schema([
+                    core_schema.str_schema(),
+                    core_schema.no_info_plain_validator_function(cls.validate),
+                ])
+            ]),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda x: str(x)
+            ),
+        )
+
+    @classmethod
+    def validate(cls, value) -> ObjectId:
+        if not ObjectId.is_valid(value):
+            raise ValueError("Invalid ObjectId")
+
+        return ObjectId(value)
 
 
 class PitchLocation(BaseModel):
@@ -18,7 +46,7 @@ class TurfType(str, Enum):
 
 
 class Pitch(BaseModel):
-    id: Optional[ObjectId] = Field(alias='_id', default=None)
+    id: Optional[PyObjectId] = Field(alias='_id', default=None)
     name: str
     location: PitchLocation
     turf_type: TurfType
@@ -30,5 +58,5 @@ class Pitch(BaseModel):
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {
-            ObjectId: str
+            PyObjectId: str
         }
--- pitch-health/views.py
+++ pitch-health/views.py
@@ -1,6 +1,8 @@
 from typing import List
+from bson import ObjectId
 
 from fastapi import APIRouter
+from pymongo import ReturnDocument
 
 from .database import db
 from .schema import Pitch
@@ -20,9 +22,26 @@ async def list_pitches() -> List[Pitch]:
 
 
 @pitches_router.post('')
-async def create_user(pitch: Pitch) -> Pitch:
+async def create_pitch(pitch: Pitch) -> Pitch:
     if hasattr(pitch, 'id'):
         delattr(pitch, 'id')
     ret = db.pitches.insert_one(pitch.model_dump(by_alias=True))
     pitch.id = ret.inserted_id
     return pitch
+
+
+@pitches_router.patch('{pitch_id}')
+async def update_pitch(pitch_id: str, pitch_dict: dict) -> Pitch:
+    result = db.pitches.find_one_and_update(
+        filter={'_id': ObjectId(pitch_id)},
+        update={'$set': pitch_dict},
+        return_document=ReturnDocument.AFTER
+    )
+    return Pitch(**result)
+
+
+@pitches_router.delete('{pitch_id}', status_code=204)
+async def delete_pitch(pitch_id: str) -> None:
+    db.pitches.find_one_and_delete(
+        filter={'_id': ObjectId(pitch_id)}
+    )
